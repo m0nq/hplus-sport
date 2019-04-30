554cfef03e349e497e261410568f93913b083afe
ildev <dev@industriallogic.com>
2019-04-30 12:42:47 -0700

    Replace react components by appending or replacing corresponding data

--- js/script.js
+++ js/script.js
@@ -14,6 +14,89 @@
   let state = {};
   let category = 'all';
 
+  const updateActivityList = event => {
+//		if (event !== undefined && $(this).hasClass('selected')) {
+    if (event !== undefined && event.target.classList.contains('selected')) {
+      // if the 'event' parameter is defined, then a tab has been clicked; if not, then this is the
+      //   default case and the view simply needs to be updated
+      // if the clicked tab has the class 'selected', then no need to change location of 'selected' class
+      //   or change the DOM
+      return true;
+//		} else if (event !== undefined && !$(this).hasClass('selected')) {
+    } else if (event !== undefined && !event.target.classList.contains('selected')) {
+      // if the 'event' parameter is defined, then a tab has been clicked
+      // if the clicked tab does not have the class 'selected', then location of 'selected' class must be added
+      //   to the clicked element and removed from its siblings
+//			category = $(this).attr('id');
+      category = event.target.id;
+
+//			$('.options div').removeClass('selected');
+
+      document.querySelectorAll('.options div').forEach(el => {
+        el.classList.remove('selected');
+      });
+
+//			$(this).addClass('selected');
+      event.target.classList.add('selected');
+    }
+
+    const updateState = type => {
+      switch (category) {
+        case 'solo':
+          state.activities.push(...activities[`solo${type}`]);
+          break;
+        case 'team':
+          state.activities.push(...activities[`team${type}`]);
+          break;
+        default:
+          state.activities.push(...activities[`solo${type}`]);
+          state.activities.push(...activities[`team${type}`]);
+          break;
+      }
+    };
+
+    state.activities = [];
+    if (state.condition === 'Rain') {
+      updateState('In');
+    } else if (state.condition === 'Snow' || state.degFInt < 50) {
+      updateState('OutCold');
+    } else {
+      updateState('OutWarm');
+    }
+
+//		const $into = $('.activities')[0];
+    const into = document.querySelector('.activities');
+
+//		ReactDOM.render(<Activities {...state} />, $into);
+//     ReactDOM.render(<Activities {...state} />, into);
+
+    // function Activities(props) {
+    //   const activitiesList = props.activities.map(function (activity, index) {
+    //     return <li key={index}>{activity}</li>;
+    //   });
+    //   return (
+    //     <div>
+    //       <ul>{activitiesList}</ul>
+    //     </div>
+    //   );
+    // }
+
+    let activitiesContainer = document.createElement('div');
+    let list = document.createElement('ul');
+    state.activities.forEach((activity, index) => {
+      let listItem = document.createElement('li');
+      list.setAttribute('key', index);
+      listItem.textContent = activity;
+      list.appendChild(listItem);
+    });
+    activitiesContainer.appendChild(list);
+    document.querySelector('.activities div') ?
+      into.replaceChild(activitiesContainer, document.querySelector('.activities div')) :
+      into.appendChild(activitiesContainer);
+
+    $('.results').slideDown(300);
+  };
+
   // handle ajax success
   const updateUISuccess = response => {
     const degC = response.main.temp - 273.15;
@@ -51,6 +134,12 @@
     let iconImage = document.createElement('img');
     iconImage.setAttribute('src', state.icon);
     iconImage.setAttribute('alt', state.condition);
+    conditionsPara.appendChild(iconImage);
+    container.appendChild(cityPara);
+    container.appendChild(conditionsPara);
+    document.querySelector('.conditions div') ?
+      into.replaceChild(container, document.querySelector('.conditions div')) :
+      into.appendChild(container);
 
     updateActivityList();
   };
@@ -90,81 +179,4 @@
   });
 
   // handle selection of a new category (team/solo/all)
-  function updateActivityList(event) {
-//		if (event !== undefined && $(this).hasClass('selected')) {
-    if (event !== undefined && event.target.classList.contains('selected')) {
-      // if the 'event' parameter is defined, then a tab has been clicked; if not, then this is the
-      //   default case and the view simply needs to be updated
-      // if the clicked tab has the class 'selected', then no need to change location of 'selected' class
-      //   or change the DOM
-      return true;
-//		} else if (event !== undefined && !$(this).hasClass('selected')) {
-    } else if (event !== undefined && !event.target.classList.contains('selected')) {
-      // if the 'event' parameter is defined, then a tab has been clicked
-      // if the clicked tab does not have the class 'selected', then location of 'selected' class must be added
-      //   to the clicked element and removed from its siblings
-//			category = $(this).attr('id');
-      category = event.target.id;
-
-//			$('.options div').removeClass('selected');
-
-      document.querySelectorAll('.options div').forEach(function (el) {
-        el.classList.remove('selected');
-      });
-
-//			$(this).addClass('selected');
-      event.target.classList.add('selected');
-    }
-
-    state.activities = [];
-    if (state.condition === 'Rain') {
-      updateState('In');
-    } else if (state.condition === 'Snow' || state.degFInt < 50) {
-      updateState('OutCold');
-    } else {
-      updateState('OutWarm');
-    }
-
-    function updateState(type) {
-      switch (category) {
-        case 'solo':
-          state.activities.push(...activities[`solo${type}`]);
-          break;
-        case 'team':
-          state.activities.push(...activities[`team${type}`]);
-          break;
-        default:
-          state.activities.push(...activities[`solo${type}`]);
-          state.activities.push(...activities[`team${type}`]);
-          break;
-      }
-    }
-
-//		const $into = $('.activities')[0];
-    const into = document.querySelector('.activities');
-
-//		ReactDOM.render(<Activities {...state} />, $into);
-//     ReactDOM.render(<Activities {...state} />, into);
-
-    // function Activities(props) {
-    //   const activitiesList = props.activities.map(function (activity, index) {
-    //     return <li key={index}>{activity}</li>;
-    //   });
-    //   return (
-    //     <div>
-    //       <ul>{activitiesList}</ul>
-    //     </div>
-    //   );
-    // }
-
-    let activitiesContainer = document.createElement('div');
-    let list = document.createElement('ul');
-    state.activities.forEach((activity, index) => {
-      let listItem = document.createElement('li');
-      listItem.textContent = activity;
-      list.setAttribute('key', index);
-    });
-
-    $('.results').slideDown(300);
-  }
 })();
